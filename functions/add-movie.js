b48748bd89fdfb146887e7b5a2adae7c0325cc44
Mario Andres <marioarendon2@gmail.com>
2020-08-18 22:32:24 -0600

    User authentication

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -2,6 +2,14 @@ const { query } = require("./utils/hasura");
 
 exports.handler = async (event, context, callback) => {
   const { id, title, tagline, poster } = JSON.parse(event.body);
+  const { user } = context.clientContext;
+  const isLoggedIn = user && user.app_metadata;
+  if (!isLoggedIn || !user.app_metadata.roles.includes("admin")) {
+    return {
+      statusCode: 401,
+      body: "Unauthorized",
+    };
+  }
 
   const result = await query({
     query: `
